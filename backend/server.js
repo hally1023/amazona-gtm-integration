c7f88eb501fcd606b9c83edb03574c3cfc9130b1
Hally Sim <hally.sim@gmail.com>
2020-12-14 22:25:46 -0500

    publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import path from 'path';
 import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 import orderRouter from './routers/orderRouter.js';
@@ -22,6 +23,8 @@ app.use('/api/orders', orderRouter);
 app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
+
+const __dirname = path.resolve();
 
 // app.get('/', (req, res) => {
 //   res.send('Server is ready');
