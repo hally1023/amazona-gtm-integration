e7e5941b224dbfbfef0f93b202e8e489618c291f
Hally Sim <hally.sim@gmail.com>
2020-12-27 22:25:56 -0500

    publish to heroku

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -98,6 +98,7 @@ export default function ProductScreen(props) {
                       </li>
                       <li>
                         <button
+                          id="addToCart"
                           onClick={addToCartHandler}
                           className="primary block"
                         >
